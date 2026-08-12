9e3ea7b3dc77a4eed89d57379c97b70d2264ae47
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2026-08-12 21:19:07 +0530

    gradle: Migrate to newDsl

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.compose.compiler)
-    id("org.jetbrains.kotlin.plugin.serialization")
+    alias(libs.plugins.kotlin.serialization)
     id("kotlin-parcelize")
 }
 
